4015189d136af4895f7ef4ddb168b6484026b10e
Yousong <dbthd0220@naver.com>
2021-10-08 00:19:07 +0900

    init level2

---
+++ hash/level2/README.md
@@ -0,0 +1,3 @@
+# PROBLEM
+
+## [🦹‍♀️ Disguise](https://programmers.co.kr/learn/courses/30/lessons/42577) 
---
+++ hash/level2/solution.js
@@ -0,0 +1,13 @@
+//샘플 예제
+const clothes = [["yellowhat", "headgear"], ["bluesunglasses", "eyewear"], ["green_turban", "headgear"]];
+let answer = solution(clothes);
+
+console.log(answer);
+
+function solution(clothes) {
+    
+    answer = 0;
+    return answer;
+
+}
+
