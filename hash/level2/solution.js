623ef3471dc189666ed3fd8591074261f830bf1e
Yousong <dbthd0220@naver.com>
2021-10-11 23:13:48 +0900

    add hash level2 solution

--- hash/level2/solution.js
+++ hash/level2/solution.js
@@ -1,13 +1,29 @@
 //샘플 예제
 const clothes = [["yellowhat", "headgear"], ["bluesunglasses", "eyewear"], ["green_turban", "headgear"]];
-let answer = solution(clothes);
+const answer = solution(clothes);
 
 console.log(answer);
 
 function solution(clothes) {
-    
-    answer = 0;
-    return answer;
+
+   let answer = 1;
+
+   //의상종류
+   let categories = {};
+   for(let cloth of clothes){
+       //옷 종류별 카운트
+       if(categories[cloth[1]]){
+           categories[cloth[1]]++;
+       }else{
+           categories[cloth[1]] = 1;
+       }
+   }
+
+   for(let i of Object.keys(categories)){
+       answer *= categories[i]+1;
+   }
+
+    return answer -1;
 
 }
 
