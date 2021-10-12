033cdf0eaa3af9aa6566f14a076646a031c27e0a
Yousong <dbthd0220@naver.com>
2021-10-12 22:59:49 +0900

    init hash level3

---
+++ hash/level3/README.md
@@ -0,0 +1,3 @@
+# PROBLEM
+
+## [🎧 BEST ALBUM](https://programmers.co.kr/learn/courses/30/lessons/42579) 
---
+++ hash/level3/solution.js
@@ -0,0 +1,12 @@
+//sample
+const genres = ["classic", "pop", "classic", "classic", "pop"];
+const plays = [500, 600, 150, 800, 2500];
+
+solution(genres, plays);
+
+function solution(genres, plays) {
+
+    var answer = [];
+    return answer;
+}
+
