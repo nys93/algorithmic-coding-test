808b7b22325c8ff51d5e131048ec0ddba6a3901b
Yousong <dbthd0220@naver.com>
2021-10-13 23:25:33 +0900

    add hash level3 solution

--- hash/level3/solution.js
+++ hash/level3/solution.js
@@ -6,7 +6,41 @@ solution(genres, plays);
 
 function solution(genres, plays) {
 
-    var answer = [];
+    const groupByGenre = genres.reduce((acc, cur, index) => {
+        if (!acc[cur]) {
+            acc[cur] = {
+                playedCount: 0,
+                playList: [],
+            };
+        }
+
+        acc[cur].playedCount += plays[index];
+        acc[cur].playList.push([index, plays[index]]);
+
+        return acc;
+    }, {});
+
+    const groupByGenreList = Object.values(groupByGenre).sort(
+        (genre1, genre2) => {
+            return genre2.playedCount - genre1.playedCount;
+        },
+    );
+
+    const answer = groupByGenreList.reduce((acc, genre) => {
+        genre.playList.sort((play1, play2) => {
+            return play2[1] - play1[1];
+        });
+
+        acc.push(genre.playList[0][0]);
+
+        if (genre.playList.length > 1) {
+            acc.push(genre.playList[1][0]);
+        }
+
+        return acc;
+    }, []);
+
+
     return answer;
 }
 
