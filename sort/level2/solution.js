00b59fa2dd5c2d630c0df5f558bb05a5023e9ea1
Yousong <dbthd0220@naver.com>
2021-10-17 21:20:38 +0900

    add sort level2 solution

--- sort/level2/solution.js
+++ sort/level2/solution.js
@@ -1,9 +1,10 @@
 const numbers = [6, 10, 2];
+const answer = solution(numbers);
 
-solution(numbers);
+console.log(answer);
 
 function solution(numbers) {
 
-    const answer = '';
-    return answer;
+    const answer = numbers.map((number) => number.toString()).sort((a, b) => (b+a) - (a+b)).join("");
+    return answer.replace(/^0+/, "0");
 }
