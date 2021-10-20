4354b4a0d419b4c48fb197ee0986066b75361d6a
Yousong <dbthd0220@naver.com>
2021-10-20 23:32:12 +0900

    add sort level2-1

--- sort/level2-1/solution.js
+++ sort/level2-1/solution.js
@@ -1,4 +1,17 @@
+const citations = [3, 0, 6, 1, 5];
+solution(citations);
+
 function solution(citations) {
-    const answer = 0;
-    return answer;
+
+    // Descending order
+    citations = citations.sort(function (a, b) {
+        return b - a;
+    });
+
+    // find H-index
+    for (let i = 0; i < citations.length; i++){
+        if(i >= citations[i]) return i;
+    }
+
+    return citations.length;
 }
